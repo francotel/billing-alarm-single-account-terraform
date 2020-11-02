222b81759930b41dcdb8524e8b9687d43f8b3a3f
francotel <franco.navarrotel@.com>
2020-11-02 14:22:27 -0500

    Adding .gitignore file

--- main.tf
+++
@@ -1,9 +0,0 @@
-module "repositories" {
-  source    = "./modules/codecommit"
-  
-  for_each = var.repositories
-
-  name = each.value.name
-  description = each.value.description
-  tags = each.value.tags
-}
